bb8d55cc251bd77e0d80403de20f0b8227c5cb7e
Rakesh <Rakesh@RAKESH-PC>
2021-07-26 20:08:54 +0530

    Dao layer with 3 tables

--- PWC_FINDO_DAO/src/main/java/com/pwc/findo/dao/PwcFindoDaoApplication.java
+++ PWC_FINDO_DAO/src/main/java/com/pwc/findo/dao/PwcFindoDaoApplication.java
@@ -10,13 +10,22 @@ public class PwcFindoDaoApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx=	SpringApplication.run(PwcFindoDaoApplication.class, args);
 		
-	FindoDao dao=ctx.getBean(FindoDao.class, "fdao");
+	    DonorDAO dao=ctx.getBean(DonorDAO.class, "fdao");
+		DonationDAO donationdao=ctx.getBean(DonationDAO.class,"donationdao");
+		CampaignManagerDAO cmpdao=ctx.getBean(CampaignManagerDAO.class,"cmpdao");
+		
 		
 	//dao.saveDonorDetails();
 	//dao.getDonorDetailsbyId(1);
 	//dao.updateDonorDetails(1, "Arul", "arunaaa@123");
 	//dao.deleteDonorDetails(1);
-	dao.getAllDonorDetails();
+	//dao.getAllDonorDetails();
+	//donationdao.saveCampaignDonationDetails();
+	//	donationdao.getAllDonationDetails();
+		
+		
+	cmpdao.checkManagerDetails("rahul", "rahul123");	
+		
 	}
 
 }
--- PWC_FINDO_DAO/src/main/java/com/pwc/findo/dao/repository/DonorRepository.java
+++ PWC_FINDO_DAO/src/main/java/com/pwc/findo/dao/repository/DonorRepository.java
@@ -3,7 +3,7 @@ package com.pwc.findo.dao.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pwc.findo.dao.model.Donor;
+import com.pwc.findo.dao.entities.Donor;
 @Repository
 public interface DonorRepository extends JpaRepository<Donor,Integer> {
 
